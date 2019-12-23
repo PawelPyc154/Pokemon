e1c5819d835a01614a623871ebf68c50f39cecbb
Paweł Pyc <pawelpyc154@gmail.com>
2019-12-23 19:59:44 +0100

    add store middleware

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import './index.scss';
+import thunk from 'redux-thunk';
+import allReducers from './store/allReducers';
+import App from './App';
 
-import * as serviceWorker from './serviceWorker';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  allReducers,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
