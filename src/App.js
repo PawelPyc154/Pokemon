dd1e4d4ed673b833330229bca380bdd10dc1751b
Paweł Pyc <pawelpyc154@gmail.com>
2019-12-24 15:28:58 +0100

    add Redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import Page from './Components/Page';
 import Pagination from './Components/Pagination';
@@ -8,10 +8,9 @@ import Pagination from './Components/Pagination';
 function App() {
   return (
     <Router>
-      {/* <Switch> */}
+      <Redirect to="/1" />
       <Route path="/:page" component={Pagination} />
       <Route path="/:page" component={Page} />
-      {/* </Switch> */}
     </Router>
   );
 }
