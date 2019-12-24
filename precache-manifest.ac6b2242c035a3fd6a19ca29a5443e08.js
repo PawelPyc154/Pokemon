52e61290ed858cb6a3cfba19688ce65bda947ff8
Paweł Pyc <pawelpyc154@gmail.com>
2019-12-24 14:58:45 +0100

    Updates

---
+++ precache-manifest.ac6b2242c035a3fd6a19ca29a5443e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc7d72b7b1e94d8df7325e68bea6fc0",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "227a9900300d2c788357",
+    "url": "/Pokemon/static/css/main.3902973f.chunk.css"
+  },
+  {
+    "revision": "1fdccd7b214ca56d17c6",
+    "url": "/Pokemon/static/js/2.7f19f283.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/Pokemon/static/js/2.7f19f283.chunk.js.LICENSE"
+  },
+  {
+    "revision": "227a9900300d2c788357",
+    "url": "/Pokemon/static/js/main.49cc6b13.chunk.js"
+  },
+  {
+    "revision": "621596b7eef631e93be2",
+    "url": "/Pokemon/static/js/runtime-main.2d4901c9.js"
+  }
+]);
